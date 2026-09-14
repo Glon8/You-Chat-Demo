9de8e6c4a78c675d43b34ea9cbad687a82ed75a0
Daniel Ruz <Dani.Ruzanov@gmail.com>
2026-09-14 16:39:10 +0300

    send function optimization

--- frontend/data/send.py
+++ frontend/data/send.py
@@ -7,26 +7,33 @@ from .helpers import add_pending, err_pop
 
 def snd():
     gnr = op["gnr"]
+    ws = get_WS()
     
-    if not ld and gnr['rcv_id'] in cnt and get_WS():
-        now_time = time.time()
-        MSG = input("message > ")
+    if ws:
+        err_pop("Current WebSocket not accessible")
+        return
+    
+    if not gnr["rcv_id"]:
+        err_pop("No receiver has been selected")
+        return
+    
+    if gnr['rcv_id'] not in cnt:
+        err_pop("Unknown number check your contacts")
+        return
 
-        get_WS().send(
-            json.dumps(
-                {
-                    "snd_id": gnr["snd_id"],
-                    "rcv_id": gnr["rcv_id"],
-                    "req_type": "msg",
-                    "trn_dt": MSG,
-                    "tm_stm": now_time
-                }
-            )
+    now_time = time.time()
+    MSG = input("message > ")
+
+    ws.send(
+        json.dumps(
+            {
+                "snd_id": gnr["snd_id"],
+                "rcv_id": gnr["rcv_id"],
+                "req_type": "msg",
+                "trn_dt": MSG,
+                "tm_stm": now_time
+            }
         )
+    )
 
-        add_pending(gnr["snd_id"], gnr["rcv_id"], now_time, MSG)
-    else:
-        if gnr['rcv_id'] not in cnt:
-            err_pop("Unknown number check your contacts")
-        else:
-            err_pop("Current WebSocket not accessible")
+    add_pending(gnr["snd_id"], gnr["rcv_id"], now_time, MSG)
