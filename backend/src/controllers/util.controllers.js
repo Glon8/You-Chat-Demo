0b989245fcaee842696ab981dfdc0cd72df22980
Daniel Ruz <Dani.Ruzanov@gmail.com>
2026-09-24 12:29:39 +0300

    added additional debugging logs

--- backend/src/controllers/util.controllers.js
+++ backend/src/controllers/util.controllers.js
@@ -26,6 +26,9 @@ export const rel_upd = (req, res) => {
     if (data && data.length > 0) data.forEach(it => { if (!relays.has(it)) add_relay(it); });
 
     if (get_user(snd_id)) upd_rel_sync(snd_id);
+    //=================================== FOR DEBUG ONLY
+    console.log('[Relay Peer List]');
+    console.log(relays)
 
     res.sendStatus(204);
 }
